efe1e301b4dd3d30d078580b8086213f5278c74d
yaninacba <126228201+yaninacba@users.noreply.github.com>
2023-05-15 23:02:56 -0300

    Update cvjava.js

--- cvjava.js
+++ cvjava.js
@@ -1,6 +1,14 @@
 
-function activayDesactiva() {
+function activayDesactiva1() {
   document.getElementById("texto1").classList.toggle("show");
 }
+function activayDesactiva2() {
+  document.getElementById("texto2").classList.toggle("show");
+}
+function activayDesactiva3() {
+  document.getElementById("texto3").classList.toggle("show");
+}
 
-document.getElementById("contacto").onclick = function() {activayDesactiva()};
+document.getElementById("laboral").onclick = function() {activayDesactiva1()};
+document.getElementById("estudios").onclick = function() {activayDesactiva2()};
+document.getElementById("contacto").onclick = function() {activayDesactiva3()};
