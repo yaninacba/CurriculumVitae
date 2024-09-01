e845e6f36503ea1562fda2bbd79e34962e4b8bd3
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-09-01 03:30:37 -0300

    Update cvjava.js
    correciones

--- cvjava.js
+++ cvjava.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
 const lab=document.getElementById("laboral");
 const est=document.getElementById("estudios");
 const cont=document.getElementById("contacto");
+ const proyect=document.getElementById("proyectos");
 
+ proyect.addEventListener("click", () =>toggleVisibility("texto4"))
  cont.addEventListener("click", () => toggleVisibility("texto3"));
  est.addEventListener("click", () => toggleVisibility("texto2"));
  lab.addEventListener("click", () =>toggleVisibility("texto1"))
